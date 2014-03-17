1ac87efae73417ce199b59eef87b9ad56b74fef0
Hugo Marcelo Del Negro <h.marcelo.del.negro@outlook.com>
2014-03-17 00:33:53 -0300

    Layout changes

--- EnterpriseErp/Scripts/Site.js
+++ EnterpriseErp/Scripts/Site.js
@@ -1,4 +1,28 @@
 ﻿$(document).ready(function () {
 
+    /* Register Events */
+    $(".navlink").on("click", addNewTab);
+
+
+
+    /*functions*/
+    function addNewTab(event) {
+        /*Avoid Http Requests when clicked*/
+        event.preventDefault();
+
+        /*Add a new tab to right side*/
+        $('#righttabs').tabs('add', {
+            title: $(this).text(),
+            content: 'Tab Body',
+            closable: true,
+            tools: [{
+                iconCls: 'icon-mini-refresh',
+                handler: function () {
+                    alert('refresh');
+                }
+            }]
+        });
+    }
+
 
 });
